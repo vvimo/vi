8101b9b9423efe5c78b7332e04e427fe11908106
vvimo <lyj397456796@qq.com>
2018-02-08 16:47:11 +0800

    update

--- api/dao/index.js
+++ api/dao/index.js
@@ -1,9 +1,11 @@
 const articleDao = require('./article/index.js')
+const cardrDao = require('./card/index.js')
 const loginDao = require('./login/index.js')
 const userDao = require('./user/index.js')
 
 module.exports = {
   articleDao,
+  cardrDao,
   loginDao,
   userDao
 }
